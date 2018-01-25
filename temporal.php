0a60196d1e37f11ec09b138c54673b1dfc6a2876
Mikhael <mihaerusan@gmail.com>
2018-01-25 11:16:40 -0500

    Delete temporal.php

--- temporal.php
+++
@@ -1,9 +0,0 @@
-$yacth = "nemo2";
-$itinerary = "A";
-$part = "all";
-$howMany = 15;
-$url = "http://45.79.111.123/api.php?yacht=$yacth&itinerary=$itinerary&part=$part&howMany=$howMany";
-$client = curl_init($url);
-curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
-$response = curl_exec($client);
-$result = json_decode($response, true);
